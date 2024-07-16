a3b7b8e1a450fac113d18aa864eca3531f1fe136
alejandroechev <alejandro.echev@gmail.com>
2024-07-16 10:37:24 -0400

    Show cards in command page, with flip and blur background

---
+++ card.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", (event) => {
+    const cards = document.querySelectorAll('.card');
+
+    [...cards].forEach((card)=>{
+    card.addEventListener( 'click', function() {
+        card.classList.toggle('is-flipped');
+    });
+    });
+});
